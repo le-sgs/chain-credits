a79e2628cc110779096834926662f49d00c162c2
Sreehari GS <41389245+le-sgs@users.noreply.github.com>
2024-02-28 15:06:35 +0530

    Add files via upload

---
+++ Backups for Testnet/hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { INFURA_PROJECT_ID, PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
+};
